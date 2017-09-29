701d610f376f9c8949a07be26a2f523045386de4
Simon  Date <simondate@yahoo.co.uk>
2017-09-29 12:40:55 +0100

    s2l32

--- src/index.js
+++ src/index.js
@@ -7,24 +7,27 @@ import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDqN076FXQZwpGS0H1a4Kj7-RP2xqzQFu0';
 
-
-
-
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: []};
+    this.state = { videos: [],
+    selectedVideo: null};
+    this.videoSearch('skateboards');
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({videos: videos, selectedVideo: videos[0]});
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar/>
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
       </div>
     );
   }
